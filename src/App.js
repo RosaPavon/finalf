816cc5d434bd0b93eaf5e8b3b8521febf773c6d3
Rosa Pavón <mrosapavon@gmail.com>
2021-07-06 11:22:07 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { Form, Button, Row, Col, Alert } from "react-bootstrap";
+import { useState } from "react";
+import "./App.css";
+
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [feedback, setFeedback] = useState({ empty: true });
+
+  function enviar() {
+    fetch("http://localhost:3001/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: email, password: password }),
+    })
+      .then((res) => res.json())
+      .then(function (datos) {
+        setFeedback(datos);
+        setTimeout(() => {
+          setFeedback({ empty: true });
+        }, 5000);
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Row>
+      <Col></Col>
+      <Col>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
+          </Form.Group>
+          <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" onClick={() => enviar()}>
+            Enviar
+          </Button>
+          <Form.Group>
+            {feedback.empty ? (
+              <h1> </h1>
+            ) : (
+              <Alert variant={feedback.logged ? "success" : "danger"}>
+                {feedback.mensaje}
+              </Alert>
+            )}
+          </Form.Group>
+        </Form>
+      </Col>
+      <Col></Col>
+    </Row>
   );
 }
 
