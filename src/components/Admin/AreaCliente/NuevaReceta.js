1603422634e75f8ebf8914035abb6c46a0d25988
Rosa Pavón <mrosapavon@gmail.com>
2021-07-16 13:11:55 +0200

    ok

--- src/components/Admin/AreaCliente/NuevaReceta.js
+++ src/components/Admin/AreaCliente/NuevaReceta.js
@@ -5,8 +5,6 @@ import {Form, Button, Modal, Alert} from "react-bootstrap"
 import { Rate, Radio } from 'antd';
 import { FrownOutlined, MehOutlined, SmileOutlined } from '@ant-design/icons';
 
-
-
 function NuevaReceta(){
     var email = localStorage.getItem("emailToken")
 
@@ -32,6 +30,7 @@ function NuevaReceta(){
               <span className="lead"> {user.usuario}</span>    
             </li>              
             )}); 
+
  //----------------Crear receta---------------
  const [dificult, setDificult]=useState("")
  const [value, setValue] = useState(1)
@@ -40,25 +39,22 @@ function NuevaReceta(){
  const [receta, setReceta] = useState("");
  const [foto, setFoto] = useState("");
  const [feedbackReceta, setFeedbackReceta] = useState({ empty: true });
-
  
-            const customIcons = {
-              1: <SmileOutlined/>,
-              2: <SmileOutlined />,
-              3: <MehOutlined />,
-              4: <FrownOutlined/>,
-              5: <FrownOutlined/>,
-            };
+  const customIcons = {
+    1: <SmileOutlined/>,
+    2: <SmileOutlined />,
+    3: <MehOutlined />,
+    4: <FrownOutlined/>,
+    5: <FrownOutlined/>,
+   };
 
     const changeDif=(value)=>{
        setDificult(value)
     }
-
       
     const onChange = e => {
       setValue(e.target.value);
     };
-
 
     function nuevaReceta() {
       fetch("http://localhost:3001/crearreceta/crearNuevaReceta", {
@@ -73,8 +69,7 @@ function NuevaReceta(){
           titulo:titulo, 
           ingredientes:ingredientes,
           receta:receta,
-          foto:foto
-          
+          foto:foto          
         }),
       })
         .then((res) => res.json())
@@ -83,31 +78,27 @@ function NuevaReceta(){
           setTimeout(() => {
             setFeedbackReceta({ empty: true });
           }, 6000);
-         
-
-         
-        });
-      
+          setDificult("")
+          setValue(1)
+          setTitulo("")
+          setIngredientes("")
+          setReceta("")
+          setFoto("")
+        });      
     }
-
-
   
   return(
       <>
-      
- 
-       <h3 className="text-lg font-medium leading-6 text-gray-900"  >{nombreUsuario}</h3> 
+        <h3 className="text-lg font-medium leading-6 text-gray-900"  >{nombreUsuario}</h3> 
         <div className="mt-5 md:mt-0 md:col-span-2">
         <div className="shadow sm:rounded-md sm:overflow-hidden">            
         <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
-
         <div>
           <h3>Crea tu receta</h3>
           <h5>Categoría</h5>
-        <form className="row g-3 needs-validation" noValidate  >
+        <form className="row g-3 needs-validation" noValidate>
         
-      <Radio.Group onChange={onChange} value={value}>
-      
+      <Radio.Group onChange={onChange} value={value}>      
       
       <Radio value={"Entrantes"}>Entrantes</Radio>
       <Radio value={"Acompañamiento"}>Acompañamiento</Radio>
@@ -119,11 +110,9 @@ function NuevaReceta(){
      
       <Radio value={"Verduras y Legumbres"}>Verduras y Legumbres</Radio>
       <Radio value={"Pescado y Mariscos"}>Pescado y Mariscos</Radio>
-      <Radio value={"Postres y helados"}>Postres y helados</Radio>
-      
+      <Radio value={"Postres y helados"}>Postres y helados</Radio>    
      
-    </Radio.Group>
-   
+    </Radio.Group>  
   
   </form>
   </div>
@@ -134,30 +123,29 @@ function NuevaReceta(){
   <br></br>
   <br></br>
           
-    <Form>
-
+  <Form>
   <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
   <h5>Título</h5>
-    <Form.Control type="text" placeholder="Pon un título resultón"  onChange={(e) => setTitulo(e.target.value)}
-      value={titulo}/>
+  <Form.Control type="text" placeholder="Pon un título resultón"  onChange={(e) => setTitulo(e.target.value)}
+    value={titulo}/>
   </Form.Group>
   <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1"  >
   <h5>Ingredientes</h5>
-    <Form.Control as="textarea" rows={3}  onChange={(e) => setIngredientes(e.target.value)}
+  <Form.Control as="textarea" rows={3}  onChange={(e) => setIngredientes(e.target.value)}
     value={ingredientes} />
   </Form.Group>
   <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea2">
   <h5>Paso a paso</h5>
-    <Form.Control as="textarea" rows={6}  onChange={(e) => setReceta(e.target.value)}
-      value={receta} />
+  <Form.Control as="textarea" rows={6}  onChange={(e) => setReceta(e.target.value)}
+    value={receta} />
   </Form.Group>
-  <Form.Group controlId="formFile" className="mb-3">
-    <Form.Label>Pon una imagen del resultado</Form.Label>
-    <Form.Control type="file" onChange={(e) => setFoto(e.target.value)}
-      value={foto}/>
+  <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+  <h5>Pon una imagen</h5>
+  <Form.Control type="text" placeholder="www.ejemploimagen.es"  onChange={(e) => setFoto(e.target.value)}
+    value={foto}/>
   </Form.Group>
-</Form>
-<Modal.Footer>
+  </Form>
+  <Modal.Footer>
      <Form.Group>
        {feedbackReceta.empty ? (
            <h1> </h1>
@@ -170,17 +158,11 @@ function NuevaReceta(){
      <Button variant="primary" 
      onClick={() => nuevaReceta()}>
         Crear
-     </Button>    
-           
-    </Modal.Footer>
-       
-             
+     </Button>               
+    </Modal.Footer>              
         </div>          
-      </div>
-    
-  </div>
-
-
+      </div>    
+  </div> 
   </>
   )
 }
